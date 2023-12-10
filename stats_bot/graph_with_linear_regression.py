43467c498e9fa8c4106aeb4f20c7bca4320b6fb7
Ulansyn Toktorbai uulu <100096643+ulansyn@users.noreply.github.com>
2023-12-10 22:39:00 +0600

    Update graph_with_linear_regression.py

--- stats_bot/graph_with_linear_regression.py
+++ stats_bot/graph_with_linear_regression.py
@@ -1,24 +1,76 @@
-# Этот код создает график, который может быть полезен для визуализации изменений в весе 
-# и выделения тенденций в данных. Линия регрессии помогает увидеть общий тренд в изменении веса,
-# а синяя зона подчеркивает разброс между максимальным и минимальным весом. 
 import matplotlib.pyplot as plt
 import numpy as np
+from datetime import datetime, timedelta
+import json
 PATH = 'PATH'
 
-arr1 = [] # массив для хранения максимального веса в течение дня
-arr2 = [] # массив для хранения минимального веса в течение дня
-arr3 = [] 
-for i in range(len(arr2)):
-    arr3.append((arr1[i]+arr2[i])/2)
-indices = np.arange(len(arr3))
-coefficients = np.polyfit(indices, arr3, 1)
+def fuller(json_data):
+    def full_date(data):
+        # Преобразуем даты в формат datetime
+        date_format = "%d.%m.%y"
+        dates = [datetime.strptime(date, date_format) for date in data.keys()]
+
+        # Найдем минимальную и максимальную даты
+        min_date = min(dates)
+        max_date = max(dates)
+
+        # Создадим пустые массивы для недостающих дат
+        current_date = min_date
+        while current_date <= max_date:
+            formatted_date = current_date.strftime(date_format)
+            if formatted_date not in data:
+                data[formatted_date] = []
+            current_date += timedelta(days=1)
+        return data
+
+    def fill_empty_arrays(data):
+        for i in range(1, len(sorted_dates)):
+            current_date = sorted_dates[i].strftime(date_format)
+            if not data[current_date]:
+                prev_non_empty = None
+                for j in range(i - 1, -1, -1):
+                    if data[sorted_dates[j].strftime(date_format)]:
+                        prev_non_empty = sorted_dates[j]
+                        break
+
+                next_non_empty = None
+                for j in range(i + 1, len(sorted_dates)):
+                    if data[sorted_dates[j].strftime(date_format)]:
+                        next_non_empty = sorted_dates[j]
+                        break
+
+                if prev_non_empty and next_non_empty:
+                    delta = (data[next_non_empty.strftime(date_format)][0] - data[prev_non_empty.strftime(date_format)][0]) / (
+                            (next_non_empty - prev_non_empty).days or 1)
+                    data[current_date] = [
+                        data[prev_non_empty.strftime(date_format)][0] + delta * (sorted_dates[i] - prev_non_empty).days]
+
+    json_data = full_date(json_data)
+    date_format = "%d.%m.%y"
+    dates = [datetime.strptime(date, date_format) for date in json_data.keys()]
+
+    sorted_dates = sorted(dates)
+
+    sorted_data = {date.strftime(date_format): json_data[date.strftime(date_format)] for date in sorted_dates}
+
+    fill_empty_arrays(sorted_data)
+
+    return sorted_data
+
+with open('json', 'r', encoding='utf-8') as file:
+    json_data = fuller(json.load(file))
+
+print(json_data)
+arr = [] 
+for i in json_data:
+    arr.append(json_data[i][0])
+
+indices = np.arange(len(arr))
+coefficients = np.polyfit(indices, arr, 1)
 regression_line = np.polyval(coefficients, indices)
 plt.figure(figsize=(12, 7))
 plt.plot(indices, regression_line, label='линия регрессии', color='gray', linewidth=1.5, alpha=0.75)
-plt.plot(arr1, label='maximum weight', color='blue', linestyle='-', linewidth=1)
-plt.plot(arr2, label='minimum weight', color='blue', linestyle='-', linewidth=1)
-plt.plot(arr3, label='average weight', color='red', linestyle='--', linewidth=1)
-plt.fill_between(range(len(arr1)), arr1, arr2, color='blue', alpha=0.1)
+plt.plot(arr, label='average weight', color='red', linestyle='--', linewidth=1)
 
 plt.legend()
 
@@ -28,3 +80,4 @@ plt.ylabel('y')
 
 plt.grid(True)
 plt.savefig(PATH, dpi=250)
+plt.show()
